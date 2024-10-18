3a596b494ff77c872dfa791ac8f2dc9502218684
batumzff <batuhanmuzafferoglu@gmail.com>
2024-10-18 14:26:06 +0300

    updated

--- client/src/pages/LOGIN/Login.jsx
+++ client/src/pages/LOGIN/Login.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import ErrorPage from "../../components/ERROR-PAGE/ErrorPage";
 import AuthForm from "../../components/AUTH-FORM/AuthForm";
-import { Stack } from "@mui/material";
+import Stack  from "@mui/material/Stack";
 import ErrorModal from "../../components/ERROR-MODAL/ErrorModal";
 
 const Login = () => {
-  // const { error } = useSelector((state) => state.auth);
- 
+
   return (
-      <Stack sx={{justifyContent:"center", alignItems: "center", marginTop:"4rem"}}>
-       {/* <ErrorPage/> */}
-       <ErrorModal/>
-          {/* <Stack sx={{justifyContent:"center", alignItems: "center"}}> */}
-            <AuthForm formType={"login"} schema={"loginSchema"}/>
-          {/* </Stack> */}
-        
-      </Stack>
-    
+    <Stack sx={{flexDirection:"row", justifyContent: "center", alignItems: "center" }}>
+      <ErrorModal />
+      <AuthForm formType={"login"} schema={"loginSchema"} />
+    </Stack>
   );
 };
 
